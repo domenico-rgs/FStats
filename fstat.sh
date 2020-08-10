6c5aa8b269e2d2901e8ab7180482e08b2ea8d3b9
Domenico Ragusa <domenico.ragusa787@gmail.com>
2020-08-10 11:01:51 +0200

    sistemate opzioni + path con spazio

--- fstat.sh
+++ fstat.sh
@@ -1,19 +1,21 @@
 #! /bin/bash
-
-#Faccio il parse delle opzioni con getopt
-ARG=`getopt s:a: "$@"`
-eval set -- "$ARG"
+list=$(find $2 -maxdepth 1 -not -type d) 
 
 data_histogram(){
+	OIFS="$IFS"
+	IFS=$'\n'
 	for x in $list
 	do
-		((size[`wc -c < ${x}`]++)); #incremento il vettore in corrispondenza della dimensione del file -> file di 189K => size[189]++
+		((size[`wc -c < "${x}"`]++)); #incremento il vettore in corrispondenza della dimensione del file -> file di 189K => size[189]++
 	done
+	IFS="$OIFS"
 
 	print_data_histogram
 }
 
 age_histogram(){
+	OIFS="$IFS"
+	IFS=$'\n'
 	for x in $list
 	do
 		date_file=`stat --format=%Y ${x}`
@@ -37,20 +39,20 @@ age_histogram(){
 			((age[5]++));
 		fi
 	done
+	IFS="$OIFS"
 	
 	print_age_histogram
 }
 
 print_data_histogram(){
-echo "Data Histogram"
-echo "**************"
+printf "\e[38;5;045mData Histogram\n**************\n\n\033[0m"
 
 	for x in ${!size[*]} #itero su tutti gli indici del vettore che sono stati creati
 	do
 		printf "%-15s |" "${x} B" #uso la printf e non echo per aver 5 spazi dopo la stampa della dimensione cosi i "|" sono tutti allineati
 		for((i=${size[$x]}; i>0; i--)) #itero per ogni elemento del vettore per stampare l'istogramma con "*"
 		do
-			echo -n "*"
+			printf "\e[38;5;045m*\033[0m"
 		done
 		echo "" #a capo
 	done
@@ -59,39 +61,34 @@ echo "**************"
 print_age_histogram(){
 declare -a d_name=("oggi" "ieri" "ultimi 7 giorni" "ultimi 30 giorni" "ultimo anno" "vecchi")
 
-echo "Age Histogram"
-echo "*************"
+printf "\e[38;5;075mAge Histogram\n*************\n\n\033[0m"
 	for x in ${!age[*]} 
 	do
 		printf "%-20s |" "${d_name[$x]}"
 		for((i=${age[$x]}; i>0; i--))
 		do
-			echo -n "*"
+			printf "\e[38;5;075m*\033[0m"
 		done
 		echo ""
 	done
 }
 
-case "$1" in
-	#rivedere assolutamente opzioni
-	-s)	list=$(find $2 -not -type d) #prendo tutti i file compresi quelli nelle subdirectory
-		data_histogram
+while getopts "Rsa:" opt; do
+      case ${opt} in
+      	R)	list=$(find $2 -not -type d) ;;
+	s)	data_histogram
 		;;
-	-a)	list=$(find $2 -maxdepth 1 -not -type d) 
-		age_histogram
+	a)	age_histogram
 		;;	
-	-b) 	list=$(find $2 -not -type d) #prendo tutti i file compresi quelli nelle subdirectory
-		age_histogram
-		;;
-	-l)	list=$(find $2 -maxdepth 1 -not -type d) #prendo tutti i file che però non stanno nelle subdirectory
-		data_histogram
-		;;
-	--)	list=$(find $2 -maxdepth 1 -not -type d) #prendo tutti i file che però non stanno nelle subdirectory
-		data_histogram
-		echo ""
-		age_histogram
-		;;
-	*) 	echo "Errore inaspettato" 
-		exit 1
-		;;
-esac
+	\? )	echo "Invalid Option: -$OPTARG" 1>&2
+          	exit 1
+          	;;
+        : )	echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+          	exit 1
+          	;;
+        *)	echo "Internal error!"
+     		exit 1
+    		 ;;
+    esac
+done
+shift $((OPTIND - 1))
