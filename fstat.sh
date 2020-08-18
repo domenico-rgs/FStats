779b11e197e17261673f06618f33041b663625e2
Domenico Ragusa <domenico.ragusa787@gmail.com>
2020-08-18 14:57:29 +0200

    gestione eccezioni

--- fstat.sh
+++ fstat.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
-list=$(find $2 -maxdepth 1 -not -type d) 
+clear
+string="find $2 -maxdepth 1 -not -type d"
 
 data_histogram(){
 	OIFS="$IFS"
 	IFS=$'\n'
 	for x in $list
 	do
-		((size[`wc -c < "${x}"`]++)); #incremento il vettore in corrispondenza della dimensione del file -> file di 189K => size[189]++
+		if test -r ${x}; then 
+			((size[`wc -c < "${x}"`]++)); #incremento il vettore in corrispondenza della dimensione del file -> file di 189K => size[189]++
+		else
+			echo "${x} non ha i permessi necessari - ignorato"
+		fi
 	done
 	IFS="$OIFS"
 
@@ -23,17 +28,13 @@ age_histogram(){
 		
 		if [[ $date_file -ge $midnight_date ]]; then #oggi
 			((age[0]++));
-		fi
-		if [[ $date_file -ge $midnight_date-86400 && $date_file -lt $midnight_date ]]; then #ieri
+		elif [[ $date_file -ge $midnight_date-86400 && $date_file -lt $midnight_date ]]; then #ieri
 			((age[1]++));
-		fi
-		if [[ $date_file -ge $midnight_date-604800 ]]; then #ultimi 7 giorni
+		elif [[ $date_file -ge $midnight_date-604800 &&  $date_file -lt $midnight_date-86400 ]]; then #ultimi 7 giorni
 			((age[2]++));
-		fi
-		if [[ $date_file -ge $midnight_date-18144000 ]]; then #ultimi 30 giorni
+		elif [[ $date_file -ge $midnight_date-18144000 && $date_file -lt $midnight_date-604800 ]]; then #ultimi 30 giorni
 			((age[3]++));
-		fi
-		if [[ $date_file -ge $midnight_date-31536000 ]]; then #ultimo anno
+		elif [[ $date_file -ge $midnight_date-31536000 && $date_file -ge $midnight_date-18144000 ]]; then #ultimo anno
 			((age[4]++));
 		else #vecchio
 			((age[5]++));
@@ -45,23 +46,25 @@ age_histogram(){
 }
 
 print_data_histogram(){
-printf "\e[38;5;045mData Histogram\n**************\n\n\033[0m"
+printf "\e[38;5;045mIstogramma dimensione\n**************\n\n\033[0m"
 
 	for x in ${!size[*]} #itero su tutti gli indici del vettore che sono stati creati
 	do
-		printf "%-15s |" "${x} B" #uso la printf e non echo per aver 5 spazi dopo la stampa della dimensione cosi i "|" sono tutti allineati
-		for((i=${size[$x]}; i>0; i--)) #itero per ogni elemento del vettore per stampare l'istogramma con "*"
+		printf "%-15s |" "${x} B" #-15s mi allinea | a 15 spazi di distanza dall'inizio del testo
+		for((i=${size[$x]}; i>0; i--)) #itero per ogni elemento del vettore per stampare l'istogramma con gli * corrispondenti al valore dell'elemento
 		do
 			printf "\e[38;5;045m*\033[0m"
 		done
 		echo "" #a capo
 	done
+	echo ""
 }
 
+#funzionamento uguale a print_data_histogram()
 print_age_histogram(){
-declare -a d_name=("oggi" "ieri" "ultimi 7 giorni" "ultimi 30 giorni" "ultimo anno" "vecchi")
+declare -a d_name=("oggi" "ieri" "ultima settimana" "ultimo mese" "ultimo anno" "vecchi")
 
-printf "\e[38;5;075mAge Histogram\n*************\n\n\033[0m"
+printf "\e[38;5;075mIstogramma ultima modifica\n*************\n\n\033[0m"
 	for x in ${!age[*]} 
 	do
 		printf "%-20s |" "${d_name[$x]}"
@@ -71,22 +74,37 @@ printf "\e[38;5;075mAge Histogram\n*************\n\n\033[0m"
 		done
 		echo ""
 	done
+	echo ""
 }
 
+if [ $# -lt 2 ]; then
+	list=`find $1 -maxdepth 1 -not -type d`
+	if [ -n "$list" ];then
+		data_histogram
+		age_histogram
+	fi
+	exit 0
+fi
 while getopts "Rsa:" opt; do
+list=`${string}`
+if [ -z "$list" ];then
+	exit 1;
+fi
+
       case ${opt} in
-      	R)	list=$(find $2 -not -type d) ;;
+      	R)	string="find $2 -not -type d" #con -R scansiono anche i file nelle sotto directory
+      		;;
 	s)	data_histogram
 		;;
 	a)	age_histogram
 		;;	
-	\? )	echo "Invalid Option: -$OPTARG" 1>&2
+	\? )	echo "Opzione non valida: -$OPTARG" 1>&2
           	exit 1
           	;;
-        : )	echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+        : )	echo "Opzione non valida: -$OPTARG richiede un argomento" 1>&2
           	exit 1
           	;;
-        *)	echo "Internal error!"
+        *)	echo "Errore interno!"
      		exit 1
     		 ;;
     esac
